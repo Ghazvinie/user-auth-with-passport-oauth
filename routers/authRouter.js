6e3eec7a1a828b46ee69f6205efe0945ba891851
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-16 18:11:13 +0100

    finished google login

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,7 @@
 const authRouter = require('express').Router();
 const { signUpGet, signUpPost, signInGet, signInPost, googleGet, googleRedirect, dashboardGet, signOutGet } = require('../controllers/authController');
 const ensureAuthenticated  = require('../middleware/authMiddleware');
-const passport = require('passport');
+
 authRouter.get('/signup', signUpGet);
 
 authRouter.post('/signup', signUpPost);
@@ -10,35 +10,9 @@ authRouter.get('/signin', signInGet);
 
 authRouter.post('/signin', signInPost);
 
-// authRouter.get('/google', passport.authenticate('google', {
-//     scope: ['profile']
-//   }));
-
 authRouter.get('/google', googleGet);
 
-// authRouter.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/'}),
-//   function(req, res) {
-//     // Explicitly save the session before redirecting!
-//     console.log(req.user);
-//     // setTimeout(() => {
-//     //     req.session.save(() => {
-//             res.redirect('./dashboard');
-//     //       })
-//     // }, 10000)
-
-//   });
-
 authRouter.get('/google/redirect', googleRedirect);
-
-// authRouter.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/' }),
-// function(req, res) {
-//   // Successful authentication, redirect home.
-//   res.redirect('/dashboard');
-// });
-
-// authRouter.get('/google/redirect', (req, res) => {
-//     res.send('hello')
-// })
 
 authRouter.get('/dashboard', ensureAuthenticated, dashboardGet);
 
